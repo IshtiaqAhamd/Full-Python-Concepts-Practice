9c61838bdec18d27df89f3fce2972d954e15b86c
Ishtiaq Ahmad <ishtiaq.uaar55@gmail.com>
2022-05-14 14:30:45 -0700

    ishtiaq

---
+++ main.py
@@ -0,0 +1,13 @@
+percentage = int(input("Enter The Student Percentage % Marks:- "))
+if 80 <= percentage <= 100:
+    print("Excellent Percentage")
+elif 70 <= percentage <= 79:
+    print("Good Percentage")
+elif 50 <= percentage <= 69:
+    print("Average Percentage")
+elif 33 <= percentage <= 49:
+    print("Low Percentage")
+elif percentage > 100:
+    print("Invalid Percentage")
+else:
+    print("Fail")
